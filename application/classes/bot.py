d937e7bd1aeecdc83207fb984139dc5e38268e9b
Alexsander Uglin <uglin76@mail.ru>
2021-05-21 10:57:47 +0300

    refactoring

--- application/classes/bot.py
+++ application/classes/bot.py
@@ -13,49 +13,77 @@ class Bot:
         self.vk_api = authorize.get_api()
         self.image = "application/images/logo.jpg"
 
-    @staticmethod
-    def keyboard():
-        keyboard = VkKeyboard(one_time=True)
-        keyboard.add_button('Привет', color=VkKeyboardColor.PRIMARY)
-        keyboard.add_button('Поиск', color=VkKeyboardColor.SECONDARY)
-        keyboard.add_line()
-        keyboard.add_button('Послать фото', color=VkKeyboardColor.POSITIVE)
-        keyboard.add_button('Пока', color=VkKeyboardColor.NEGATIVE)
-        keyboard.add_line()
-        keyboard.add_openlink_button('Дипломное задание',
-                                     link='https://github.com/netology-code/py-advanced-diplom/tree/new_diplom')
-        return keyboard.get_keyboard()
-
     def start(self):
         for event in self.longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-                received_message = event.text.lower()
-                sender_id = event.user_id
-                sender_name = self.get_user_name(sender_id)
+                message = self.response_manager(event)
+                self.send_message(**message)
 
-                # отправка фото
-                attachments = list()
-                upload_image = self.upload.photo_messages(photos=self.image)[0]
-                attachments.append(f'photo{upload_image.get("owner_id")}_{upload_image.get("id")}')
+    def response_manager(self, event):
+        received_message = event.text.lower()
+        sender_name = self.get_user_name(event.user_id)
+        sender_id = dict(sender_id=event.user_id)
 
-                if received_message == "привет":
-                    self.send_message(sender_id, f'Добрый день, {sender_name}',  keyboard=self.keyboard())
-                elif received_message == 'послать фото':
-                    self.send_message(sender_id, 'Держи фоточку', attachments)
-                else:
-                    self.send_message(sender_id, 'Неизвестная команда.')
+        if received_message in ('начать', 'привет', 'зравствуй', 'здоров', 'старт', 'погнали', 'hi'):
+            message = f'Приветствую тебя, {sender_name}!\n' \
+                      f'Отправь мне одну из этих команд:\n' \
+                      f'Поиск, Картинка или Пока.\n\n' \
+                      f'Ну или воспользуйся клавиатуркой &#128521;\n' \
+                      f'Ах, а клавиатурки то и нет, но не беда.\n' \
+                      f'Отправь мне команду Клавиши.'
+            return dict(**sender_id, message=message)
 
-    def send_message(self, sender_id, message, attachments='', keyboard=''):
+        elif received_message == 'клавиши':
+            return dict(**sender_id, message='А вот и кнопочки', keyboard=self.show_keyboard())
+
+        elif received_message == 'спрячь клавиатуру':
+            message = 'Клавитура убрана.\n' \
+                      'Помни про команду Клавиши.'
+            return dict(**sender_id, message=message, keyboard=self.hide_keyboard())
+
+        elif received_message == 'поиск':
+            return dict(**sender_id, message='Данная функция находится в разработке')
+
+        elif received_message == 'пока':
+            return dict(**sender_id, sticker_id='3143')
+
+        elif received_message == 'картинка':
+            attachments = list()
+            upload_image = self.upload.photo_messages(photos=self.image)[0]
+            attachments.append(f'photo{upload_image.get("owner_id")}_{upload_image.get("id")}')
+            return dict(**sender_id, message='Держи картиночку', attachments=attachments)
+
+        else:
+            message = 'Неизвестная команда.\n Доступные команды:\n' \
+                      'Поиск, Картинка, Клавиши, Пока, Спрячь клавиатуру'
+            return dict(**sender_id, message=message)
+
+    def send_message(self, sender_id='', message='', attachments='', keyboard='', sticker_id=''):
         self.vk_api.messages.send(
             peer_id=sender_id,
             message=message,
             random_id=get_random_id(),
             attachment=','.join(attachments),
-            keyboard=keyboard
+            keyboard=keyboard,
+            sticker_id=sticker_id
         )
 
     def get_user_name(self, user_id):
         return self.vk_api.users.get(user_id=user_id)[0].get('first_name')
 
-    def test(self):
-        self.send_message(8301129, 'Hi from net!')
+    @staticmethod
+    def show_keyboard():
+        keyboard = VkKeyboard(one_time=False)
+        keyboard.add_button('Поиск', color=VkKeyboardColor.PRIMARY)
+        keyboard.add_button('Картинка', color=VkKeyboardColor.PRIMARY)
+        keyboard.add_button('Пока', color=VkKeyboardColor.NEGATIVE)
+        keyboard.add_line()
+        keyboard.add_button('Спрячь клавиатуру', color=VkKeyboardColor.POSITIVE)
+        keyboard.add_line()
+        keyboard.add_openlink_button('RICKROLL', link='https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+        return keyboard.get_keyboard()
+
+    @staticmethod
+    def hide_keyboard():
+        keyboard = VkKeyboard.get_empty_keyboard()
+        return keyboard
