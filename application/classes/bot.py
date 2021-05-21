66ed837330564bc4d23a0ac02c59100d8bafb1b0
Alexsander Uglin <uglin76@mail.ru>
2021-05-21 13:43:41 +0300

    refactoring

--- application/classes/bot.py
+++ application/classes/bot.py
@@ -17,12 +17,11 @@ class Bot:
         for event in self.longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
                 message = self.response_manager(event)
-                self.send_message(**message)
+                self.send_message(sender_id=event.user_id, **message)
 
     def response_manager(self, event):
         received_message = event.text.lower()
         sender_name = self.get_user_name(event.user_id)
-        sender_id = dict(sender_id=event.user_id)
 
         if received_message in ('начать', 'привет', 'зравствуй', 'здоров', 'старт', 'погнали', 'hi'):
             message = f'Приветствую тебя, {sender_name}!\n' \
@@ -31,32 +30,32 @@ class Bot:
                       f'Ну или воспользуйся клавиатуркой &#128521;\n' \
                       f'Ах, а клавиатурки то и нет, но не беда.\n' \
                       f'Отправь мне команду Клавиши.'
-            return dict(**sender_id, message=message)
+            return dict(message=message)
 
         elif received_message == 'клавиши':
-            return dict(**sender_id, message='А вот и кнопочки', keyboard=self.show_keyboard())
+            return dict(message='А вот и кнопочки', keyboard=self.show_keyboard())
 
         elif received_message == 'спрячь клавиатуру':
             message = 'Клавитура убрана.\n' \
                       'Помни про команду Клавиши.'
-            return dict(**sender_id, message=message, keyboard=self.hide_keyboard())
+            return dict(message=message, keyboard=self.hide_keyboard())
 
         elif received_message == 'поиск':
-            return dict(**sender_id, message='Данная функция находится в разработке')
+            return dict(message='Данная функция находится в разработке')
 
         elif received_message == 'пока':
-            return dict(**sender_id, sticker_id='3143')
+            return dict(sticker_id='3143')
 
         elif received_message == 'картинка':
             attachments = list()
             upload_image = self.upload.photo_messages(photos=self.image)[0]
             attachments.append(f'photo{upload_image.get("owner_id")}_{upload_image.get("id")}')
-            return dict(**sender_id, message='Держи картиночку', attachments=attachments)
+            return dict(message='Держи картиночку', attachments=attachments)
 
         else:
             message = 'Неизвестная команда.\n Доступные команды:\n' \
                       'Поиск, Картинка, Клавиши, Пока, Спрячь клавиатуру'
-            return dict(**sender_id, message=message)
+            return dict(message=message)
 
     def send_message(self, sender_id='', message='', attachments='', keyboard='', sticker_id=''):
         self.vk_api.messages.send(
